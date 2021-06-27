46c7456a7bca288b95b43600962766d40928a01d
Antoine212 <74153251+Antoine212@users.noreply.github.com>
2021-06-27 19:08:54 +0200

    Update main.py

--- Episode 0/main.py
+++ Episode 0/main.py
@@ -9,4 +9,4 @@ async def on_ready(): # When the bot is online
     print("Bot Ready !")
     await client.change_presence(activity = discord.Activity(type = discord.ActivityType.listening, name = "Cool Music !")) # "Listening to Cool Music !" < in status
 
-client.run("YOUR TOKEN GO HERE")
+client.run("ODU4NzQ5NTUwNzk3MTI3Njgw.YNiq1w.GNmT5RwfwUoE2hNZf6DZuv0TxEA")
